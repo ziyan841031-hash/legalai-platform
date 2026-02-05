723b94c128282c768d84ddfb3f3c10c1dc4699d7
ziyan841031-hash <ziyan841031@gmail.com>
2026-02-05 17:29:55 +0800

    Switch Redis client to Jedis

--- legalai-framework/src/main/java/com/legalai/platform/framework/security/RedisConfig.java
+++ legalai-framework/src/main/java/com/legalai/platform/framework/security/RedisConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
@@ -14,7 +14,7 @@ public class RedisConfig {
     @Bean
     @ConditionalOnProperty(prefix = "spring.data.redis", name = "host")
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(); // 使用 Lettuce 连接
+        return new JedisConnectionFactory(); // 使用 Jedis 连接
     }
 
     // 创建 StringRedisTemplate
