555e447faf52aab7131b6144e9a4ff0dc9c5a069
ziyan841031-hash <ziyan841031@gmail.com>
2026-02-05 17:24:24 +0800

    Make RedisConfig conditional on redis host

--- legalai-framework/src/main/java/com/legalai/platform/framework/security/RedisConfig.java
+++ legalai-framework/src/main/java/com/legalai/platform/framework/security/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.legalai.platform.framework.security;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,12 +12,14 @@ public class RedisConfig {
 
     // 创建 Redis 连接工厂
     @Bean
+    @ConditionalOnProperty(prefix = "spring.data.redis", name = "host")
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(); // 使用 Lettuce 连接
     }
 
     // 创建 StringRedisTemplate
     @Bean
+    @ConditionalOnProperty(prefix = "spring.data.redis", name = "host")
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         return new StringRedisTemplate(redisConnectionFactory); // 注入连接工厂
     }
