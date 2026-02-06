33797dd40e363828e129a51ac6ec6b7da654c923
ziyan841031-hash <ziyan841031@gmail.com>
2026-02-06 13:05:52 +0800

    Scan system mappers in admin app

--- legalai-admin/src/main/java/com/legalai/platform/admin/AdminApplication.java
+++ legalai-admin/src/main/java/com/legalai/platform/admin/AdminApplication.java
@@ -1,9 +1,11 @@
 package com.legalai.platform.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.legalai.platform")
+@MapperScan("com.legalai.platform.system.mapper")
 public class AdminApplication {
     // 主启动方法
     public static void main(String[] args) {
